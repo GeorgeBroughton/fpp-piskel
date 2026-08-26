b55a34fd671db4a7a5fe75fc5ca998102a991bee
George Daniel Broughton <george.daniel.broughton@gmail.com>
2026-08-26 15:22:48 +0100

    initial upload
    made skelet man

---
+++ PiskelLauncher.php
@@ -0,0 +1,15 @@
+<?php
+
+class PiskelLauncher extends FPPPlugin
+{
+    public function __construct()
+    {
+        $this->pluginName = "PiskelLauncher";
+    }
+
+    public function Init()
+    {
+        // Nothing yet — future you can add fseq generation here
+        return true;
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once("/opt/fpp/www/common.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Piskel Editor</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="../piskel/index.html"></iframe>
+</body>
+</html>
